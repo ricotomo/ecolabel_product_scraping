cef140499aa1b6344d5902ef3a4e0e2db1700e9e
Moritz Walther <friedicecream1@gmail.com>
2022-08-04 12:04:23 +0200

    EPEAT scraper fully implemented and working

--- scraper.py
+++ scraper.py
@@ -18,6 +18,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 import pandas as pd
 
+import requests
+
 #path to files
 file_path="C:/Users/morit/Documents/School/MSc - Fintech Thesis/Code"
 
@@ -40,7 +42,27 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 #initialize label
 label=None
 
+##EPEAT special functions
+## EPEAT offers a csv export function of the product catalogue. Because using this will be more robust than crawling all the data a script is setup to get and handle the exported file. 
 
+def excelHandlerEPEAT(link):
+    try:
+        with requests.Session() as s:
+            download = s.get(link)
+            open("data.xlsx", "wb").write(download.content)
+            df = pd.read_excel("data.xlsx", engine='openpyxl')
+    except:
+        logging.error("unable to download excel file from EPEAT")
+        return None
+    #products are listed by EPEAT for each country they are sold in. This leads to duplicate products. Drop these.
+    df = df.drop_duplicates(subset='Product Name', keep="first")
+    #the product list includes non-actively certified products. Drop these.
+    df = df[df.Status == "Active"]
+    #the product list also includes archived products. Drop these.
+    df.drop(df.loc[df['Archived On']!="*"].index, inplace=True)
+    #return only a list of product names
+    df = df[['Product Name', 'Manufacturer']]
+    return df
 
 ##Generic functions
 
@@ -60,6 +82,8 @@ def getWebsite(label=label):
             logging.warning("Exception while trying to close the cookies dialogue")
     elif label == "TCO":
         driver.get("https://tcocertified.com/product-finder/")
+    elif label == "EPEAT":
+        driver.get("https://epeat.net/")
     else:
         logging.error("The label name was not recognized or the website couldnt be retrieved via the getWebsite method")
         return Error
@@ -82,6 +106,8 @@ def getCategories(label=label):
         categories = driver.find_elements_by_class_name("m-bep_categories__childlink")
     elif label == "TCO":
         categories = driver.find_elements_by_xpath("//div[@class='col-4 col-md-3 mb-4 mb-md-0 p-md-4 category text-uppercase text-center']/a")
+    elif label == "EPEAT":
+        categories = driver.find_elements_by_xpath("//div[@id='search-tabs']/ul/li/a/span")
     else:
         return None    
     for category in categories:
@@ -113,6 +139,7 @@ def getProductPages(categoryPages, label=label):
         elif label == "TCO":
             product_table = driver.find_element_by_css_selector('table')
             elements=[]
+            #PLACEHOLDER get pages and iterate over them
             for row in product_table.find_elements_by_css_selector('tr'):
                 #scroll to row in table
                 actions = ActionChains(driver)
@@ -137,9 +164,18 @@ def getProductPages(categoryPages, label=label):
                 #driver.find_element_by_xpath('//*[contains(text(), "Close")]').click()
                 # for cell in row.find_elements_by_tag_name('td'):
                 #     print(cell.text)
+        elif label=="EPEAT":
+            ##EPEAT inclued placeholder pages for product categories they dont have yet. These are not searchable. Skip these with a continue.
+            try:
+                driver.find_element_by_xpath("//button[text()='Search']").click()
+                element = driver.current_url
+                product_pages_list.append([page[0], element])
+            except:
+                continue
         else:
             elements = None
-        for element in elements:
+        if label != "EPEAT":
+            for element in elements:
                 if label =="TCO":
                     product_pages_list.append([page[0], element])
                 else:
@@ -162,7 +198,12 @@ def getProductDetails(productPages, label=label):
             name = driver.find_element_by_xpath("//table/tbody/tr[1]/td[2]").text + " " + driver.find_element_by_xpath("//table/tbody/tr[2]/td[2]").text
             category = page[0]
             product_details_list.append([category, name])
-
+        elif label == "EPEAT":
+            #print(driver.find_element_by_xpath("//a[@class='link-icon -export']").get_attribute('href'))
+            names = excelHandlerEPEAT(driver.find_element_by_xpath("//a[@class='link-icon -export']").get_attribute('href'))
+            for index, row in names.iterrows():
+                category = page[0]
+                product_details_list.append([category, row['Manufacturer'] + " " + row['Product Name']])
     return product_details_list
 
 
@@ -206,23 +247,35 @@ def getProductDetails(productPages, label=label):
 # df.to_csv(file_path+"/product_database.csv", mode='a', header= False, index=False, encoding="utf-8")
 
 ##TCO Certified
-label ="TCO"
+# label ="TCO"
+# getWebsite(label)
+# TCO_Categories=getCategories(label)
+# print(TCO_Categories)
+# TCO_Category_Pages = getCategoryPages(TCO_Categories, label)
+# print(TCO_Category_Pages)
+# TCO_Product_Pages=getProductPages(TCO_Category_Pages, label)
+# print(TCO_Product_Pages)
+# TCO_Product_Details=getProductDetails(TCO_Product_Pages, label)
+# print(TCO_Product_Details)
+
+# # ##Write TCO products to csv file
+# df = pd.DataFrame(TCO_Product_Details)
+# df.columns = ['Category', 'Product']
+# df['Label'] = "TCO Certified"
+# df.to_csv(file_path+"/product_database.csv", mode='a', header= False, index=False, encoding="utf-8")
+
+## EPEAT
+
+label ="EPEAT"
 getWebsite(label)
-TCO_Categories=getCategories(label)
-print(TCO_Categories)
-TCO_Category_Pages = getCategoryPages(TCO_Categories, label)
-print(TCO_Category_Pages)
-TCO_Product_Pages=getProductPages(TCO_Category_Pages, label)
-print(TCO_Product_Pages)
-TCO_Product_Details=getProductDetails(TCO_Product_Pages, label)
-print(TCO_Product_Details)
-
-# ##Write TCO products to csv file
-df = pd.DataFrame(TCO_Product_Details)
-df.columns = ['Category', 'Product']
-df['Label'] = "TCO Certified"
-df.to_csv(file_path+"/product_database.csv", mode='a', header= False, index=False, encoding="utf-8")
-
+EPEAT_Categories=getCategories(label)
+print(EPEAT_Categories)
+EPEAT_Category_Pages = getCategoryPages(EPEAT_Categories, label)
+print(EPEAT_Category_Pages)
+EPEAT_Product_Pages=getProductPages(EPEAT_Category_Pages, label)
+print(EPEAT_Product_Pages)
+EPEAT_Product_Details=getProductDetails(EPEAT_Product_Pages, label)
+print(EPEAT_Product_Details)
 
 print("finished executing")
 driver.close()
